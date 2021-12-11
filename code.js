9f11906f2c905c5c851d9d4677097e840bff6a56
Benny Yarmolovich <someonesomewheremax@gmail.com>
2021-12-11 02:51:52 +0200

    Update code.js

--- code.js
+++ code.js
@@ -1,14 +1,4 @@
 if(window.snake)snake.skull_poison = function() {
-  const a = new Image();
-  a.src = 'https://www.google.com/logos/fnbx/snake_arcade/keys.svg';
-  a.width = a.height = 25;
-  a.style = 'position:relative;left:460px;top:11.5px;';
-
-  window.________b = document.createElement('div');
-  ________b.style = 'width:25px;position:relative;left:490px;top:-24px;font-size:20px;font-family:Roboto,Arial,sans-serif;'
-
-  document.getElementsByClassName('sEOCsb')[0].appendChild(a);
-  document.getElementsByClassName('sEOCsb')[0].appendChild(________b);
 
   const scripts = document.getElementsByTagName('script');
   for(let script of scripts) {
@@ -20,9 +10,7 @@ if(window.snake)snake.skull_poison = function() {
       if(code.indexOf('trophy') === -1)return;
     
     eval(
-        code.match(
-          /[a-zA-Z0-9_]{1,6}=function\(a,b\){return a.[a-zA-Z0-9]{1,4}===b}/
-        )[0].replace(
+        code.replace(
           'v4/apple_"+((10>a?"0":"")+a)+".png',
           `snake_arcade/v12/trophy_10.png
           `
